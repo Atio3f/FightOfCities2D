3f94ad9632b6adaa35bd69859b22db245e601273
Atio3f <mathiasfer38@gmail.com>
2025-09-26 15:17:03 +0200

    Logic of event

--- scripts/gameLogic/classes/AbstractEvent.gd
+++ scripts/gameLogic/classes/AbstractEvent.gd
@@ -1,2 +1,6 @@
 extends Node
 class_name AbstractEvent
+##An event contains 1 background image, 1 starting text,
+## 1 choice text with 2-5 options toggled depending of a condition,
+## each option contains a condition required, a short text to display on choice, 1 final text when you choiced it, reward list and next mission value
+## Maybe each option will be an option on the subclasses
